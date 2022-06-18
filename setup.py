9d9f90a99ac864dbbded05f6fe381ca5a68ed2e4
unknown <pallavi.anand90@gmail.com>
2022-06-18 12:20:56 +0530

    files are updated

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from typing import List
+
+
+#declearing all the variables
+PROJECT_NAME = "housing-predictor"
+VERSION = "0.0.1"
+AUTHOR =  "pallavi anand"
+DESCRIPTION = "This is my first end-to-end machine learning project"
+REQUIREMENT_FILE_NAME = "requirements.txt" 
+
+
+def get_requirements_list()->List[str]:
+   '''
+    Description: This function is going to return list of requirement which is mention
+    in requirement.txt file.
+
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+
+   '''
+   with open(REQUIREMENT_FILE_NAME) as requirement_file:
+       return requirement_file.readlines().remove("-e .")
+
+
+setup(
+name = PROJECT_NAME,
+version = VERSION,
+author = AUTHOR,
+description = DESCRIPTION,
+package = find_packages(),
+install_require = get_requirements_list()
+
+)
